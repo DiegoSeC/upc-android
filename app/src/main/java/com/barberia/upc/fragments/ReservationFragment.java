515ed8d976fc54d03db97ac910148c63b0825b0c
Diego Seminario Capulian <diegoasc@gmail.com>
2018-06-23 14:52:49 -0500

    Creating Barber Service

--- app/src/main/java/com/barberia/upc/fragments/ReservationFragment.java
+++ app/src/main/java/com/barberia/upc/fragments/ReservationFragment.java
@@ -3,17 +3,37 @@ package com.barberia.upc.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.barberia.upc.barberupc.R;
+import com.barberia.upc.models.Barber;
+import com.barberia.upc.rest.BarberService;
+import com.barberia.upc.util.Session;
+import com.barberia.upc.util.TokenInterceptor;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReservationFragment extends Fragment {
+
+    Session session;
+
+    Retrofit retrofit;
+
+    BarberService barberService;
 
 
     public ReservationFragment() {
@@ -25,7 +45,47 @@ public class ReservationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservation, container, false);
+        View view = inflater.inflate(R.layout.fragment_reservation, container, false);
+
+        session = new Session(view.getContext());
+        String token = session.getToken();
+
+        TokenInterceptor tokenInterceptor = new TokenInterceptor(token);
+        OkHttpClient client = tokenInterceptor.makeClient();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://upc.diegoseminario.com")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        barberService = retrofit.create(BarberService.class);
+
+        this.getBarbers();
+
+        return view;
     }
+
+    public void getBarbers() {
+        Call<List<Barber>> call = barberService.getAllBarber();
+        call.enqueue(cbBarber());
+    }
+
+    public Callback<List<Barber>> cbBarber() {
+        return new Callback<List<Barber>>() {
+            @Override
+            public void onResponse(Call<List<Barber>> call, Response<List<Barber>> response) {
+                List<Barber> barbers = response.body();
+                Log.d("BABERS", barbers.get(0).getName());
+            }
+
+            @Override
+            public void onFailure(Call<List<Barber>> call, Throwable t) {
+
+            }
+        };
+    }
+
+
 
 }
